2b02b533befd7f30801038aeaf7091dc371ef150
Geoffrey Mayhew <geoff.mayhew@noaa.gov>
2024-02-12 15:53:16 -0800

    Actually added in gdrive_versions functions, not just a line calling it

--- functions.R
+++ functions.R
@@ -582,4 +582,49 @@ gdrive_download <- function(local_path, gdrive_dribble, ver = NULL) {
 #======================================================================================================================#
 
 #' *View the revision history of a file on the Gdrive*, including the modification dates, modifying user, and size.
-gdrive_versions("new_data_test2.rdata", v2_dribble)
+gdrive_versions <- function(gdrive_file, gdrive_dribble){
+  # Make the dribble of the gdrive_file
+  gdrive_item <- googledrive::with_drive_quiet(
+    googledrive::drive_get(
+      path = paste0(gdrive_dribble$path, gdrive_file), 
+      shared_drive = googledrive::as_id(gdrive_dribble$shared_drive_id)
+    )
+  )
+  
+  if( nrow(gdrive_item) == 0 ) {
+    stop(paste0(
+      "No file named ", crayon::yellow(gdrive_file), " found in gdrive folder ", 
+      crayon::yellow(gdrive_dribble$path), "."
+    ))
+  }
+  
+  # Get the revision list
+  revision_lst <- googledrive::do_request(
+    googledrive::request_generate(
+      endpoint = "drive.revisions.list", params = list(fileId = gdrive_item$id, fields = "*")
+    )
+  )$revisions
+  
+  # Subset the information of the versions to useful information
+  rev_info <- lapply(revision_lst, "[", c("modifiedTime", "size", "keepForever"))
+  rev_user <- lapply(lapply(revision_lst, "[[", "lastModifyingUser"), "[[", "displayName")
+  rev_info <- mapply(function(x, y) append(x, c(modifiedBy = y)), x = rev_info, y = rev_user, SIMPLIFY = F)
+  
+  # Format the modified date
+  for(i in seq_along(rev_info)) {
+    rev_info[[i]]$modifiedTime <- format(
+      as.POSIXct(rev_info[[i]]$modifiedTime , format = "%Y-%m-%dT%H:%M:%OSZ", tz = "GMT" , origin = "1970-01-01"),
+      tz = Sys.timezone(), usetz = T
+    )
+  }
+  
+  # Convert to data.frame
+  rev_info_df <- do.call(rbind, lapply(rev_info , as.data.frame))
+  rev_info_df$Version <- seq_along(rev_info)
+  rev_info_df <- rev_info_df[, c("Version", "modifiedTime", "modifiedBy", "size", "keepForever")]
+  
+  # Outputs
+  cat(crayon::yellow(paste0(v2_dribble$path, gdrive_file)), "\n")
+  rev_info_df[rev(seq_along(rev_info)), ]
+  
+}
